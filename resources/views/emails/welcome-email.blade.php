947b1eb9e27fd74aa201c126299af52e242f9488
DanilSidorov1737 <dannyboy1737@gmail.com>
2023-05-11 17:14:00 -0400

    InstaGram Clone

---
+++ resources/views/emails/welcome-email.blade.php
@@ -0,0 +1,10 @@
+@component('mail::message')
+# Introduction
+
+This is a community of Fellow developers and we love that you hae joined us.
+
+
+Thanks,<br>
+Danil S.
+{{ config('app.name') }}
+@endcomponent
